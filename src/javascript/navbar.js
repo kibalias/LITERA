22c5089a713b4054f55e226d8076d7f17e237f94
kibalias <74294078+kibalias@users.noreply.github.com>
2023-12-06 03:32:41 +0800

    added active class in navbar

--- src/javascript/navbar.js
+++ src/javascript/navbar.js
@@ -1,7 +1,9 @@
 /*
     This javascript file put solid color style into the nav
-    upon reaching services section
+    upon reaching services section and setting active link
 */
+const sections = document.querySelectorAll('section');
+const links = document.querySelectorAll('.nav-link');
 const navbar = document.getElementById('navbar');
 const serviceSection = document.getElementById('services');
 window.addEventListener('scroll', triggerNavStyle);
@@ -11,11 +13,31 @@ function triggerNavStyle() {
         Prerequisite: scrolled (css) property and styling
     */
    
-    if(window.scrollY >= serviceSection.offsetTop){
+    if(window.scrollY >= serviceSection.offsetTop-100){
         // scrollY indicates the range of the scroll vertically from the top of the page,
         // and if in or greater than the service section outer border, will add the style
         navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
     }
+
+    setActiveLink();
+}
+
+function setActiveLink() {
+    let currentSection = "";
+    sections.forEach(section => {
+        // on scroll and upon view the next section, set active link
+        if(window.scrollY >= (section.offsetTop)){
+            currentSection = section.id;
+        }
+    });
+    // match the id in the link to set the active class
+    links.forEach(link => {
+        link.classList.remove('active');
+        if(link.getAttribute('href').includes('#' + currentSection)){
+            link.classList.add('active');
+            console.log(link.getAttribute('href'));
+        }
+    });
 }
