bded82bf4632e3c803f674df56bc32a220ccccf1
kibalias <74294078+kibalias@users.noreply.github.com>
2023-12-05 23:38:27 +0800

    separated data in another file

---
+++ src/javascript/data.js
@@ -0,0 +1,108 @@
+export const testimonies = [
+    {
+        author: 'B.D.', 
+        review: 'The stylists are not only talented but also incredibly friendly. They transformed my hair into a work of art. The personalized attention and the modern yet cozy atmosphere make each visit a delightful experience. I\'ve finally found a salon that understands and enhances my unique style.',
+        img: 'assets/images/pexels-cottonbro-studio-3993463.jpg'
+    }, 
+    {
+        author: 'E.X.', 
+        review: 'The expertise and attention to detail displayed by their stylists are unparalleled. From the chic haircuts to the relaxing spa treatments, every visit leaves me feeling rejuvenated and stylish. Highly recommended!',
+        img: 'assets/images/pexels-cottonbro-studio-4004470.jpg'
+    },
+    {
+        author: 'J.V.', 
+        review: 'The team exceeded my expectations. They listened to my preferences, suggested fantastic ideas, and delivered flawless results. The ambiance is calming, and the service is top-notch. My confidence has soared since I started coming here!',
+        img: 'assets/images/pexels-cottonbro-studio-3997376.jpg'
+    },
+    {
+        author: 'A.J.', 
+        review: 'I had such an amazing experience in this salon. My hair feels luscious.',
+        img: 'assets/images/pexels-rdne-stock-project-7755461.jpg'
+    },
+]
+export const services = [
+    {
+        icon: '<iconify-icon icon="wpf:cut" width="3rem" style="color: white;"></iconify-icon>',
+        name: 'CUT/STYLE'
+    },
+    {
+        icon: '<iconify-icon icon="mdi:pot-mix" width="3rem" style="color: white;"></iconify-icon>',
+        name: 'COLOR'
+    },
+    {
+        icon: '<iconify-icon icon="icon-park-outline:hair-clip" width="3rem" style="color: white;"></iconify-icon>',
+        name: 'Extensions'
+    },
+    {
+        icon: '<iconify-icon icon="mdi:bottle-tonic" width="3rem" style="color: white;"></iconify-icon>',
+        name: 'Texture/ Treatments'
+    },
+    {
+        icon: '<iconify-icon icon="mingcute:eyebrow-fill" width="3rem" style="color: white;"></iconify-icon>',
+        name: 'Makeup, Lash, & Brow'
+    },
+    {
+        icon: '<iconify-icon icon="icon-park-solid:nail-polish" width="3rem" style="color: white;"></iconify-icon>',
+        name: 'Nails'
+    },
+    {
+        icon: '<iconify-icon icon="icon-park-solid:towel" width="3rem" style="color: white;"></iconify-icon>',
+        name: 'Waxing'
+    },
+    {
+        icon: '<iconify-icon icon="material-symbols-light:spa" width="3rem" style="color: white;"></iconify-icon>',
+        name: 'Spa'
+    },
+]
+export const branches = [
+    {
+        place: 'Sampaloc',
+        services: 'All Services',
+        hours:'8:00 AM - 5:00 PM',
+        contact: {
+            tel: '(02) 633-157', 
+            cel: '(+639) 900-105-3167',
+            loc: 'Avida Towers, Espana, Sampaloc, Manila'
+        }
+    },
+    {
+        place: 'Quezon City',
+        services: 'All Services',
+        hours:'8:00 AM - 5:00 PM',
+        contact: {
+            tel: '(02) 673-820', 
+            cel: '(+639) 900-346-8641',
+            loc: 'Regalla, Cubao, Quezon City'
+        }
+    },
+    {
+        place: 'Antipolo',
+        services: 'All Services',
+        hours:'10:00 AM - 5:00 PM',
+        contact: {
+            tel: '(02) 633-123', 
+            cel: '(+639) 900-235-9764',
+            loc: '123 Penafrancia, Antipolo, Rizal'
+        }
+    },
+    {
+        place: 'Marilao',
+        services: 'All Services',
+        hours:'10:00 AM - 5:00 PM',
+        contact: {
+            tel: '(44) 633-405', 
+            cel: '(+639) 900-235-8105',
+            loc: '183 Sta. Rosa, Marilao, Bulacan'
+        }
+    },
+    {
+        place: 'Dagupan',
+        services: 'All Services',
+        hours:'8:00 AM - 5:00 PM',
+        contact: {
+            tel: '(075) 633-450', 
+            cel: '(+639) 900-235-8203',
+            loc: '11 Arellano St., Dagupan City, Pangasinan'
+        }
+    },
+]
