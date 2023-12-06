e6d1d4e21727eb2ab5cbc5d23d272db8303c62e5
kibalias <74294078+kibalias@users.noreply.github.com>
2023-12-06 12:08:13 +0800

    manually minify css

--- src/javascript/navbar.js
+++ src/javascript/navbar.js
@@ -8,19 +8,14 @@ const navbar = document.getElementById('navbar');
 const serviceSection = document.getElementById('services');
 window.addEventListener('scroll', triggerNavStyle);
 
-function triggerNavStyle() {
-    /*
-        Prerequisite: scrolled (css) property and styling
-    */
-   
+function triggerNavStyle() { 
     if(window.scrollY >= serviceSection.offsetTop-100){
         // scrollY indicates the range of the scroll vertically from the top of the page,
-        // and if in or greater than the service section outer border, will add the style
+        // and if in or greater than the 100px before the section outer border, will add the style
         navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
     }
-
     setActiveLink();
 }
 
@@ -32,7 +27,7 @@ function setActiveLink() {
             currentSection = section.id;
         }
     });
-    // match the id in the link to set the active class
+    // check if the link id has the current section id and set the active class
     links.forEach(link => {
         link.classList.remove('active');
         if(link.getAttribute('href').includes('#' + currentSection)){
--- src/javascript/script.js
+++ src/javascript/script.js
@@ -14,8 +14,8 @@ function appendToDiv () {
     Prerequisite: div where data will be appended
     Method functionality: Loop to the testimonies, get index, and display
     */
-    if(testimonies.length <= 0 && services.length <= 0) {
-        // check if testimonies is empty, if true, display error
+    if(testimonies.length <= 0 && services.length <= 0 && branches.length <= 0) {
+        // check if object is empty, if true, display error
         alert("Cannot retrieve or display data.");
         return;
     }
@@ -23,8 +23,8 @@ function appendToDiv () {
         testimony_placeholder.innerHTML +=
         `<div class="testimony col-12 col-lg-6">
             <div class="testimony-container">
-                <div class="image">
-                    <img src="`+testimony.img+`" alt="testimony-`+index+`">
+                <div class="image-placeholder">
+                    <img src="`+testimony.img+`" alt="testimony-`+(index+1)+`" loading="lazy">
                 </div>
                 <div class="review">
                     <p>"`+testimony.review+`"</p>
@@ -34,10 +34,6 @@ function appendToDiv () {
         </div>`
     });
 
-    /*
-    Prerequisite: div where data will be appended
-    Method functionality: Loop to the services, and display
-    */
    services.forEach(service => {
     services_placeholder.innerHTML +=
     `<div class="services-item col-6 col-md-3 ">
@@ -48,15 +44,10 @@ function appendToDiv () {
     </div>`
    });
 
-   /*
-    Prerequisite: div where data will be appended
-    Method functionality: Loop to the branches, and display to table
-    */
    branches.forEach(branch => {
-    
     branches_placeholder.innerHTML += 
         `<tr>
-            <th class><div class="branch-loc">`+branch.place+`</div><img src="`+branch.img+`"></th>
+            <th class><div class="branch-loc">`+branch.place+`</div><img src="`+branch.img+`" loading="lazy"></th>
             <td><div class="branch-info">`+createListOfService(branch.services)+`</div></td>
             <td>`+branch.hours+`</td>
             <td><div class="branch-info"><p>`+branch.contact.tel+`</p><p>`+branch.contact.cel+`</p><p>`+branch.contact.loc+`</p></div></td>
@@ -65,17 +56,15 @@ function appendToDiv () {
 }
 
 function createListOfService(branch) {
-    // method called when inserting branch into innerhtml
-   let htmlSyntax = "";
-
+    // method called when inserting each branch services into innerhtml
+   let branchServices = "";
     // check if the data passed is not an object, if not, return
     if(typeof branch !=="object"){
         return branch;
     }
-
     // if data is an object, access through Object values, append in <p> and retun
    Object.values(branch).forEach((service) => {
-        htmlSyntax+= `<p>`+service+`</p>`;
+        branchServices+= `<p>`+service+`</p>`;
    })
-   return htmlSyntax;
+   return branchServices;
 }
